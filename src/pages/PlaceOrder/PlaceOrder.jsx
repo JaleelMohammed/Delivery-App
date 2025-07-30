0d69d90e84c14a0c87f3ad40d5b2049d94f25095
JaleelMohammed <iamjaleelmohammed@gmail.com>
2025-07-30 12:08:22 +0530

    changed

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -8,20 +8,20 @@ const PlaceOrder = () => {
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First Name' />
-          <input type="text" placeholder='Last Name' />
+          <input type="text" placeholder='First Name' required/>
+          <input type="text" placeholder='Last Name' required />
         </div>
-        <input type="Email" placeholder='Email Address' />
-        <input type="text" placeholder='Street' />
+        <input type="Email" placeholder='Email Address'required />
+        <input type="text" placeholder='Street' required/>
         <div className="multi-fields">
-          <input type="text" placeholder='City' />
-          <input type="text" placeholder='State' />
+          <input type="text" placeholder='City' required/>
+          <input type="text" placeholder='State'required />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip code' />
-          <input type="text" placeholder='Country' />
+          <input type="text" placeholder='Zip code' required/>
+          <input type="text" placeholder='Country' required/>
         </div>
-        <input type="text " placeholder='phone' />
+        <input type="text " placeholder='phone'required />
       </div>
       <div className="place-order-right">
       <div className="cart-total">
